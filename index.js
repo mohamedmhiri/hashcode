350ce72f3201d2a584ba1a6fbeb726a73af1a4a5
MHIRI Mohamed <mmhiri@amaris.com>
2019-02-28 16:36:23 +0100

    tom and mush nbrs => minNbrIngredients & maxTotalCellsNbr

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ let file = fs.readFileSync('./input/exple.txt', 'utf8');
 let firstInputRow = file.split('\n')[0];
 let rowsNbr = firstInputRow.split(' ')[0];
 let colNbr = firstInputRow.split(' ')[1];
-let tomNbr = firstInputRow.split(' ')[2];
-let mushNbr = firstInputRow.split(' ')[3];
+let minNbrIngredients = firstInputRow.split(' ')[2];
+let maxTotalCellsNbr = firstInputRow.split(' ')[3];
 console.log(`rowsNbr: ${rowsNbr}`);
 console.log(`colNbr: ${colNbr}`);
-console.log(`tomNbr: ${tomNbr}`);
-console.log(`mushNbr: ${mushNbr}`);
+console.log(`minNbrIngredients: ${minNbrIngredients}`);
+console.log(`maxTotalCellsNbr: ${maxTotalCellsNbr}`);
 
-fs.writeFileSync('./output/exple.txt', mushNbr, 'utf8');
+fs.writeFileSync('./output/exple.txt', maxTotalCellsNbr, 'utf8');
