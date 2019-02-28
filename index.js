03d985c498bbc5a0f9d2547dc24200b2206bab71
MHIRI Mohamed <mmhiri@amaris.com>
2019-02-28 16:27:09 +0100

    initial setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+let file = fs.readFileSync('./input/exple.txt', 'utf8');
+
+let firstInputRow = file.split('\n')[0];
+let rowsNbr = firstInputRow.split(' ')[0];
+let colNbr = firstInputRow.split(' ')[1];
+let tomNbr = firstInputRow.split(' ')[2];
+let mushNbr = firstInputRow.split(' ')[3];
+console.log(`rowsNbr: ${rowsNbr}`);
+console.log(`colNbr: ${colNbr}`);
+console.log(`tomNbr: ${tomNbr}`);
+console.log(`mushNbr: ${mushNbr}`);
