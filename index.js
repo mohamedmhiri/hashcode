6e8d3d57a80d64190c695ce2e765d49c5afc6ca0
MHIRI Mohamed <mmhiri@amaris.com>
2019-02-28 21:03:00 +0100

    first test

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const fs = require('fs');
 
-let file = fs.readFileSync(`./input/${process.argv[2]}.in`, 'utf8');
-console.log(process.argv);
+let file = fs.readFileSync(`./input/${process.argv[2]}.txt`, 'utf8');
 
 
 let firstInputRow = file.split('\n')[0];
-let rowsNbr = firstInputRow.split(' ')[0];
-let colNbr = firstInputRow.split(' ')[1];
-let minNbrIngredients = firstInputRow.split(' ')[2];
-let maxTotalCellsNbr = firstInputRow.split(' ')[3];
-console.log(`rowsNbr: ${rowsNbr}`);
-console.log(`colNbr: ${colNbr}`);
-console.log(`minNbrIngredients: ${minNbrIngredients}`);
-console.log(`maxTotalCellsNbr: ${maxTotalCellsNbr}`);
+let rowsNbr = firstInputRow;
+let tags = [];
+let slideNbr = 0;
 
-fs.writeFileSync(`./output/${process.argv[2]}.out`, maxTotalCellsNbr, 'utf8');
+let hIndex = [];
+let vIndex = [];
+
+let res = '';
+let resH = '';
+let resV = '';
+for (var line = 0; line < file.split('\n').length-1; line++) {
+    let type = file.split('\n')[line].split(' ')[0];
+    if (type == 'H') {
+        slideNbr++;
+        hIndex.push(line-1)
+        resH = `${resH}\n${line-1}`;
+    } else if (type == 'V'){
+        vIndex.push(line-1)
+        if (vIndex.length == 2) {
+            resV = `${resV}\n${vIndex[0]} ${vIndex[1]}`
+            slideNbr++;
+            vIndex = []
+        }
+
+    }
+    //let tagNbr = file.split('\n')[line].split(' ')[1];
+
+    /*    
+    for (let key = 0; key < tagNbr; key++) {
+        tags.push(file.split('\n')[line].split(' ')[key + 2]);
+    }
+    */
+}
+res = `${slideNbr}${resH}${resV}`;
+console.log(res);
+
+
+
+fs.writeFile(`./output/${process.argv[2]}.txt`, res.split('EOF')[0], 'utf8', (err, data) => {
+    
+});
+
+
